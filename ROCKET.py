f0b853722d61c8e71df4ad7593bd9fe7271027fb
WN-7401-jetlearner <saleem.akhtari@sky.com>
2026-06-16 19:50:36 +0100

    Add files via upload

--- ROCKET.py
+++ ROCKET.py
@@ -4,23 +4,19 @@ height=500
 width=600
 screen=pygame.display.set_mode((width,height))
 running=True
-galaxy=pygame.image.load("C:\\Users\muham\\OneDrive\\Pictures\\Desktop\\Jet learn WANIYA\\Python Game Developer\\images\\galaxy.png")
+
+
+galaxy=pygame.image.load("C:\\Users\muh//\\OneDrive\\Pictures\\Desktop\\Jet learn WANIYA\\Python Game Developer\\images\\galaxy.png")
 rocket=pygame.image.load("C:\\Users\\muham\\OneDrive\\Pictures\\Desktop\\Jet learn WANIYA\\Python Game Developer\\images\\sally.png")
+
 while running:
  for i in pygame.event.get():
   if i.type==pygame.QUIT:
    running=False
-  pygame.display.update()
+ pygame.display.update()
 
-  screen.blit(galaxy,(0,0))
-  screen.blit(rocket,(200,200))
-  pygame.display.update()
-  
+ screen.blit(galaxy,(0,0))
+ screen.blit(rocket,(200,200))
+ pygame.display.update()
 
-
-
-
-
-
-
-
+ /
